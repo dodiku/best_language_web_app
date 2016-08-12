6080cc3c2c60ba7e1a31aec80adc8cef4b503d83
dodiku <ayalon.dror@gmail.com>
2016-08-12 03:53:56 -0400

    getting all data from github (pagination)

--- app.js
+++ app.js
@@ -71,16 +71,17 @@ function compareForSort(a,b){
     return 1;
 }
 
-// return an array with github data
-function getGitHubData(){
-  console.log("[1]in github function");
+function getGitHubDataFirstTime(){
+  var time = timeForApi();
+  var tempArray = [];
+  var url = "http://api.github.com/search/repositories?per_page=1000&sort=stars&q=+created:"+time;
 
   return new Promise(function(resolve, reject) {
-    var time = timeForApi();
-    var tempArray = [];
 
     var options = {
-      url: "http://api.github.com/search/repositories?per_page=1000&sort=stars&q=+created:"+time,
+      // url: "http://api.github.com/search/repositories?per_page=1000&sort=stars&q=+created:"+time,
+      url: url,
+      // url: "https://api.github.com/search/repositories?per_page=1000&sort=stars&q=+created%3A2016-08-10",
       headers: {
         'User-Agent': 'best_language_web_app'
         }
@@ -88,6 +89,226 @@ function getGitHubData(){
 
     Request(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
+        var headers = response.headers;
+        // var nextLink = 0;
+        var lastPageNumber = 0;
+
+        // geting the total number of pages
+        if (headers.link.search('rel="last"') > 0){
+          // console.log("there are more pages");
+          // nextLink = headers.link.substring(1, headers.link.indexOf('>'));
+
+          lastPageNumber = headers.link;
+          var CutFrom = lastPageNumber.indexOf('rel="last"');
+          CutFrom = CutFrom - 10;
+          var CutTo = lastPageNumber.length;
+          lastPageNumber = lastPageNumber.slice(CutFrom,CutTo);
+          // console.log(lastPageNumber);
+
+          CutFrom = 4;
+          lastPageNumber = lastPageNumber.substring(5, lastPageNumber.indexOf('>'));
+          console.log(lastPageNumber);
+
+        }
+
+        if (lastPageNumber > 10) {
+          lastPageNumber = 10;
+        }
+
+        // return tempArray;
+        var gitObj = {
+          // array: tempArray,
+          // number_of_pages: lastPageNumber,
+          number_of_pages: 2,
+          // time: time,
+          url: url,
+        };
+        console.log(gitObj);
+        resolve(gitObj);
+
+      }
+      else if (error){
+        console.error(error);
+        // return tempArray;
+        reject(Error(error));
+      }
+      else {
+        console.error("GETTING DATA FROM GITHUB: FAILED");
+        // return tempArray;
+        reject(Error("GETTING DATA FROM GITHUB: FAILED"));
+      }
+
+    });
+  });
+
+}
+
+function getAllGitHubData(gitObj){
+  console.log("we're in getAllGitHubData");
+  console.log(gitObj);
+  var numberOfPages = gitObj.number_of_pages;
+  var url = gitObj.url;
+  var tempArray = [];
+  var arrayOfPromises = [];
+
+  // var array = getGitHubData(url);
+  // console.log("printing array");
+  // console.log(array);
+
+
+  // for (var i = 0; i < numberOfPages ; i++){
+  for (var i = 0; i < 3 ; i++){
+
+    var PageUrl = url + "&page=" + (i + 1);
+    console.log("running for loop on getAllGitHubData");
+    console.log(PageUrl);
+
+    gitPromise(PageUrl,tempArray)
+    // .then(function(array){
+    //   tempArray.push(array);
+    //   return Promise.resolve(tempArray);
+    // });
+    .then(function(array){
+      tempArray = array;
+      return Promise.resolve(tempArray);
+    })
+    .then(function(array){
+      count = count + 1;
+      console.log("updateing count: " + count);
+      console.log(array);
+    });
+
+
+    // if (i=2){
+    //
+    // }
+
+    // getGitHubData(PageUrl).then()
+    // arrayOfPromises.push(function(tempArray){
+    //   var array = getGitHubData(PageUrl);
+    //   console.log(array);
+    //
+    //   return Promise.resolve(tempArray);});
+
+
+      // function(tempArray){
+      // // tempArray.push(function(){return Promise.resolve(getGitHubData(url));});
+      // tempArray.push(function(){return Promise.resolve([1,2,3]);});
+      // return Promise.resolve(tempArray);
+    // });
+  }
+  // console.log("array of promises");
+  // console.log(arrayOfPromises);
+
+
+  //////////// ****************
+  //////////// ****************
+  //////////// ****************
+  // arrayOfPromises.reduce(function (prev, curr) {
+  //     return prev.then(curr);
+  // }, Promise.resolve([])).then(function(total){
+  //   console.log("NOW WHAT??...  CREATE A SINGLE ARRAY AND RETURN IT");
+  //   console.log(total);
+  // });
+  //////////// ****************
+  //////////// ****************
+  //////////// ****************
+
+
+  // return new Promise(function(resolve, reject) {
+  //   var arrayOfPromises = [];
+  //   console.log("i'm on getallgithubdata");
+  //
+  //   if (numberOfPages > 10) {
+  //     numberOfPages = 8;
+  //   }
+  //
+  //
+  //
+  //   console.log(arrayOfPromises);
+  //   Promise.all(function(){
+  //   console.log(arrayOfPromises);
+  //   return arrayOfPromises;
+  //   })
+  //   .then(function(value){
+  //     console.log(value);
+  //     resolve(value);
+  //   });
+  //
+  // });
+
+  /////
+//
+//   var myAsyncFuncs = [
+//     function (val) {return Promise.resolve(val + 1);},
+//     function (val) {return Promise.resolve(val + 2);},
+//     function (val) {return Promise.resolve(val + 3);},
+// ];
+//
+// myAsyncFuncs.reduce(function (prev, curr) {
+//     return prev.then(curr);},
+//     Promise.resolve(1))
+// .then(function (total) {
+//     console.log('RESULT is ' + total);  // prints "RESULT is 7"
+// });
+
+
+  ////
+
+
+
+
+}
+
+function gitPromise(PageUrl, tempArray){
+  var array = getGitHubData(PageUrl, tempArray);
+  return Promise.resolve(array);
+}
+
+// returns an object (ex. array) with github data and number of pages from github api
+function getGitHubData(url){
+// function getGitHubData(){
+  console.log("[1]in github function");
+  // var time = timeForApi();
+  var tempArray = [];
+
+  return new Promise(function(resolve, reject) {
+
+    var options = {
+      // url: "http://api.github.com/search/repositories?per_page=1000&sort=stars&q=+created:"+time,
+      url: url,
+      // url: "https://api.github.com/search/repositories?per_page=1000&sort=stars&q=+created%3A2016-08-10",
+      headers: {
+        'User-Agent': 'best_language_web_app'
+        }
+    };
+
+    Request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var headers = response.headers;
+        // var nextLink = 0;
+        var lastPageNumber = 0;
+
+        // geting the total number of pages
+        // if (headers.link.search('rel="last"') > 0){
+        //   // console.log("there are more pages");
+        //   // nextLink = headers.link.substring(1, headers.link.indexOf('>'));
+        //
+        //   lastPageNumber = headers.link;
+        //   var CutFrom = lastPageNumber.indexOf('rel="last"');
+        //   CutFrom = CutFrom - 10;
+        //   var CutTo = lastPageNumber.length;
+        //   lastPageNumber = lastPageNumber.slice(CutFrom,CutTo);
+        //   // console.log(lastPageNumber);
+        //
+        //   CutFrom = 4;
+        //   lastPageNumber = lastPageNumber.substring(5, lastPageNumber.indexOf('>'));
+        //   console.log(lastPageNumber);
+        //
+        // }
+        // else {
+        //   console.log("no more pages");
+        // }
         var gitData = JSON.parse(body);
         var repositories = gitData.items;
         var countMax = langNum;
@@ -97,6 +318,7 @@ function getGitHubData(){
         for (var i = 0; i < repositories.length; i++){
           var language = repositories[i].language;
           var exists = 0;
+
           if (language === null){
             continue;
           }
@@ -109,15 +331,24 @@ function getGitHubData(){
             }
           }
 
-          if(exists === 0 && countActual <= countMax) {
+          if(exists === 0) {
             tempArray.push({
               name: language,
               repos: 1
             });
-            countActual = countActual + 1;
-
           }
+
         }
+
+        // sorting the array
+        tempArray = tempArray.sort(compareForSort);
+
+        //triming the array according to langNum
+        // var trimmedArray = [];
+        // for (i=0;i<langNum;i++){
+        //   trimmedArray[i] = tempArray[i];
+        // }
+        // tempArray=trimmedArray;
 
         // summarizing github repos
         var sumRepositories = 0;
@@ -129,9 +360,6 @@ function getGitHubData(){
         for (i = 0; i < tempArray.length; i++){
           tempArray[i].repos_percent = Math.round(tempArray[i].repos / sumRepositories * 100);
         }
-
-        // sorting the array
-        tempArray = tempArray.sort(compareForSort);
 
         // done with github
         console.log("[2]GETTING DATA FROM GITHUB: SUCCEEDED. RECEIVED " + sumRepositories + " VALID RESULTS OUT OF " + repositories.length + ".");
@@ -281,7 +509,96 @@ app.get('/api/yesterday',function(req, res){
     res.json(dataArray);
   }
   else {
-    getGitHubData()
+    // getGitHubData()
+    getGitHubDataFirstTime()
+    .then(function(obj){
+      console.log("this is the the 1st .then:");
+      console.log(obj);
+      var url = obj.url;
+      var pages = obj.number_of_pages;
+      var array = [];
+      for (var i=1;i<(pages+1);i++){
+        array[i] = url + "&page=" + i;
+      }
+      console.log("array of pages numbers:");
+      console.log(array);
+
+      return Promise.all(array.map(function(value){
+        return getGitHubData(value);
+      }));
+    })
+    .then(function(array){
+      console.log("this is the array on the 2nd .then:");
+
+      var singleArray = array[1];
+
+      console.log("090909090===========BEFORE============");
+      console.log(singleArray);
+
+
+      for (var num=2;num<(array.length);num++){
+        var smallArray = array[num];
+        for (var i = 0 ; i < smallArray.length ; i++){
+          var language = smallArray[i].name;
+          var repos = smallArray[i].repos;
+          var exists = 0;
+          for (var n = 0; n < singleArray.length ; n++){
+            if (language == singleArray[n].name){
+              singleArray[n].repos = singleArray[n].repos + repos;
+              exists = 1;
+            }
+          }
+          if (exists === 0){
+            singleArray.push({
+              name: language,
+              repos: repos,
+            });
+          }
+        }
+      }
+
+      // summarizing github repos
+      var sumRepositories = 0;
+      for (num = 0; num < singleArray.length; num++){
+        sumRepositories = sumRepositories + singleArray[num].repos;
+      }
+      console.log(sumRepositories);
+
+      console.log("090909090===========AFTER SUM REPOS============");
+      console.log(singleArray);
+
+      // triming the array according to langNum
+      if (singleArray.length > langNum) {
+        var trimmedArray = [];
+        for (var x=0;x<langNum;x++){
+          trimmedArray[x] = singleArray[x];
+        }
+        singleArray=trimmedArray;
+      }
+
+      console.log("090909090===========AFTER TRIM============");
+      console.log(singleArray);
+
+      // sorting the array
+      singleArray = singleArray.sort(compareForSort);
+
+      console.log("090909090===========AFTER SORT============");
+      console.log(singleArray);
+
+
+      // calculating repos_percent
+      for (num = 0; num < singleArray.length; num++){
+        singleArray[num].repos_percent = Math.round(singleArray[num].repos / sumRepositories * 100);
+      }
+
+      console.log("090909090===========AFTER REPOS_PERCENT============");
+      console.log(singleArray);
+
+      return Promise.resolve(singleArray);
+
+    })
+    // .then(function(object){return getAllGitHubData(object);})
+    // .then(...make array...)
     .then(function(array){var i = 0; return getStackOverflowData(array, array[i].name, i);})
     .then(function(array){var i = 1; return getStackOverflowData(array, array[i].name, i);})
     .then(function(array){var i = 2; return getStackOverflowData(array, array[i].name, i);})
